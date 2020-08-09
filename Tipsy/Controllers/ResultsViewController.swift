ad127e7e56d02258b625d73c287ef2e2375a7940
Lucas <lucasbrito_9@hotmail.com>
2020-08-09 18:28:21 -0300

    Added IBoutlets and IBActions and ResultsViewController created.

---
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Lucas Santana Brito on 09/08/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
