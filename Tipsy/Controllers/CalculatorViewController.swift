122013e5d540e7e55fd0fb0102dc6736536f2b1b
Lucas <lucasbrito_9@hotmail.com>
2020-08-09 19:58:09 -0300

    Calcuate bill total.

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -19,21 +19,51 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var splitNumberLabel: UILabel!
 
     
+    // MARK: Atributos
+    var percentSelect = 0.0
+    var steppValue = 0
     
     // MARK: - IBAction
     
     @IBAction func tipChanged(_ sender: UIButton) {
+        billtextField.endEditing(true)
         
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        
+        sender.isSelected = true
+        
+        let numberPercent = percentStringToDouble(percent: sender.currentTitle ?? "0%")
+        
+        percentSelect = numberPercent / 100
     }
     
-    @IBAction func setpperValueChanged(_ sender: UIStepper) {
-        
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        steppValue = Int(sender.value)
+        splitNumberLabel.text = String(steppValue)
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        guard let bill = Double(billtextField.text ?? "0.0") else { return }
+        let tip = percentSelect * bill
+        let billTotal = (tip + bill) / Double(steppValue)
+        
+        print(String(format: "%.2f", billTotal))
+    }
+    
+    // MARK: - Functions
+    
+    private func calculate() {
         
     }
     
-    
+    private func percentStringToDouble(percent: String) -> Double {
+        let numberString = String(percent.dropLast())
+        
+        let numberPercent = Double(numberString)
+        
+        return numberPercent ?? 0.0
+    }
 }
 
